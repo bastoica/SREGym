ce7af4ce2b1f929d34ce65cb079a67b9575d195b
Yiming Su <yiming34@illinois.edu>
2025-06-02 08:39:54 -0700

    Added remote connection logic
    Signed-off-by: Yiming Su <yiming34@illinois.edu>

--- clients/langgraph-agent/tools/jaeger_tools.py
+++ clients/langgraph-agent/tools/jaeger_tools.py
@@ -10,7 +10,10 @@ from langchain_core.callbacks import CallbackManagerForToolRun
 from langchain_core.tools import tool
 from langchain_core.tools.base import ArgsSchema, BaseTool
 from mcp import ClientSession, StdioServerParameters, stdio_client
+from mcp.client.sse import sse_client
 from pydantic import BaseModel, Field
+
+USE_HTTP = True
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
@@ -47,29 +50,34 @@ class GetTraces(BaseTool):
         logger.info(
             f"calling mcp get_traces from langchain get_traces, with service {service} and operation {operation}"
         )
-        curr_dir = os.getcwd()
-        logger.info(f"current dir: {curr_dir}")
-        server_path = f"{curr_dir}/mcp_server/observability_server.py"
-        server_name = "observability"
         exit_stack = AsyncExitStack()
-        logger.info(f"Connecting to server: {server_name} at path: {server_path}")
-        is_python = server_path.endswith(".py")
-        is_js = server_path.endswith(".js")
-        if not (is_python or is_js):
-            raise ValueError("Server script must be a .py or .js file")
-
-        command = sys.executable if is_python else "node"  # Uses the current Python interpreter from the activated venv
-        server_params = StdioServerParameters(command=command, args=[server_path], env=None)
-
-        logging.info(f"Starting server: {server_name} with params: {server_params}")
-        stdio_transport = await exit_stack.enter_async_context(stdio_client(server_params))
-
-        stdio, write = stdio_transport
-        session = await exit_stack.enter_async_context(ClientSession(stdio, write))
+        server_name = "observability"
+        if USE_HTTP:
+            logger.info("Using HTTP, connecting to server.")
+            server_url = "http://127.0.0.1:9953"
+            http_transport = await exit_stack.enter_async_context(sse_client(url=server_url))
+            session = await exit_stack.enter_async_context(ClientSession(http_transport))
+        else:
+            logger.info("Not using HTTP, booting server locally, not recommended.")
+            curr_dir = os.getcwd()
+            logger.info(f"current dir: {curr_dir}")
+            server_path = f"{curr_dir}/mcp_server/observability_server.py"
+            logger.info(f"Connecting to server: {server_name} at path: {server_path}")
+            is_python = server_path.endswith(".py")
+            is_js = server_path.endswith(".js")
+            if not (is_python or is_js):
+                raise ValueError("Server script must be a .py or .js file")
+            command = (
+                sys.executable if is_python else "node"
+            )  # Uses the current Python interpreter from the activated venv
+            server_params = StdioServerParameters(command=command, args=[server_path], env=None)
+            logging.info(f"Starting server: {server_name} with params: {server_params}")
+            stdio_transport = await exit_stack.enter_async_context(stdio_client(server_params))
+            stdio, write = stdio_transport
+            session = await exit_stack.enter_async_context(ClientSession(stdio, write))
 
         await session.initialize()
         logger.info(f"Connected to server: {server_name}, adding to session dict")
-
         logger.info(f"Listing tools for server: {server_name}")
         response = await session.list_tools()
         tools = response.tools
@@ -100,25 +108,31 @@ class GetServices(BaseTool):
         run_manager: Optional[CallbackManagerForToolRun] = None,
     ) -> str:
         logger.info(f"calling mcp get_services from langchain get_services")
-        curr_dir = os.getcwd()
-        logger.info(f"current dir: {curr_dir}")
-        server_path = f"{curr_dir}/mcp_server/observability_server.py"
-        server_name = "observability"
         exit_stack = AsyncExitStack()
-        logger.info(f"Connecting to server: {server_name} at path: {server_path}")
-        is_python = server_path.endswith(".py")
-        is_js = server_path.endswith(".js")
-        if not (is_python or is_js):
-            raise ValueError("Server script must be a .py or .js file")
-
-        command = sys.executable if is_python else "node"  # Uses the current Python interpreter from the activated venv
-        server_params = StdioServerParameters(command=command, args=[server_path], env=None)
-
-        logging.info(f"Starting server: {server_name} with params: {server_params}")
-        stdio_transport = await exit_stack.enter_async_context(stdio_client(server_params))
-
-        stdio, write = stdio_transport
-        session = await exit_stack.enter_async_context(ClientSession(stdio, write))
+        server_name = "observability"
+        if USE_HTTP:
+            logger.info("Using HTTP, connecting to server.")
+            server_url = "http://127.0.0.1:9953"
+            http_transport = await exit_stack.enter_async_context(sse_client(url=server_url))
+            session = await exit_stack.enter_async_context(ClientSession(http_transport))
+        else:
+            logger.info("Not using HTTP, booting server locally, not recommended.")
+            curr_dir = os.getcwd()
+            logger.info(f"current dir: {curr_dir}")
+            server_path = f"{curr_dir}/mcp_server/observability_server.py"
+            logger.info(f"Connecting to server: {server_name} at path: {server_path}")
+            is_python = server_path.endswith(".py")
+            is_js = server_path.endswith(".js")
+            if not (is_python or is_js):
+                raise ValueError("Server script must be a .py or .js file")
+            command = (
+                sys.executable if is_python else "node"
+            )  # Uses the current Python interpreter from the activated venv
+            server_params = StdioServerParameters(command=command, args=[server_path], env=None)
+            logging.info(f"Starting server: {server_name} with params: {server_params}")
+            stdio_transport = await exit_stack.enter_async_context(stdio_client(server_params))
+            stdio, write = stdio_transport
+            session = await exit_stack.enter_async_context(ClientSession(stdio, write))
 
         await session.initialize()
         logger.info(f"Connected to server: {server_name}, adding to session dict")
@@ -151,25 +165,31 @@ class GetOperations(BaseTool):
         run_manager: Optional[CallbackManagerForToolRun] = None,
     ) -> str:
         logger.info(f"calling mcp get_operations from langchain get_operations with service {service}")
-        curr_dir = os.getcwd()
-        logger.info(f"current dir: {curr_dir}")
-        server_path = f"{curr_dir}/mcp_server/observability_server.py"
-        server_name = "observability"
         exit_stack = AsyncExitStack()
-        logger.info(f"Connecting to server: {server_name} at path: {server_path}")
-        is_python = server_path.endswith(".py")
-        is_js = server_path.endswith(".js")
-        if not (is_python or is_js):
-            raise ValueError("Server script must be a .py or .js file")
-
-        command = sys.executable if is_python else "node"  # Uses the current Python interpreter from the activated venv
-        server_params = StdioServerParameters(command=command, args=[server_path], env=None)
-
-        logging.info(f"Starting server: {server_name} with params: {server_params}")
-        stdio_transport = await exit_stack.enter_async_context(stdio_client(server_params))
-
-        stdio, write = stdio_transport
-        session = await exit_stack.enter_async_context(ClientSession(stdio, write))
+        server_name = "observability"
+        if USE_HTTP:
+            logger.info("Using HTTP, connecting to server.")
+            server_url = "http://127.0.0.1:9953"
+            http_transport = await exit_stack.enter_async_context(sse_client(url=server_url))
+            session = await exit_stack.enter_async_context(ClientSession(http_transport))
+        else:
+            logger.info("Not using HTTP, booting server locally, not recommended.")
+            curr_dir = os.getcwd()
+            logger.info(f"current dir: {curr_dir}")
+            server_path = f"{curr_dir}/mcp_server/observability_server.py"
+            logger.info(f"Connecting to server: {server_name} at path: {server_path}")
+            is_python = server_path.endswith(".py")
+            is_js = server_path.endswith(".js")
+            if not (is_python or is_js):
+                raise ValueError("Server script must be a .py or .js file")
+            command = (
+                sys.executable if is_python else "node"
+            )  # Uses the current Python interpreter from the activated venv
+            server_params = StdioServerParameters(command=command, args=[server_path], env=None)
+            logging.info(f"Starting server: {server_name} with params: {server_params}")
+            stdio_transport = await exit_stack.enter_async_context(stdio_client(server_params))
+            stdio, write = stdio_transport
+            session = await exit_stack.enter_async_context(ClientSession(stdio, write))
 
         await session.initialize()
         logger.info(f"Connected to server: {server_name}, adding to session dict")
--- mcp_server/observability_server.py
+++ mcp_server/observability_server.py
@@ -1,12 +1,19 @@
+import argparse
 import logging
 from datetime import datetime, timedelta
 from typing import Any
 
 import httpx
 import mcp.types as types
+import uvicorn
+from mcp.server import Server
 from mcp.server.fastmcp import FastMCP
 from mcp.server.fastmcp.prompts import base
+from mcp.server.sse import SseServerTransport
 from pydantic import AnyUrl
+from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.routing import Mount, Route
 from utils import ObservabilityClient
 
 logger = logging.getLogger("Observability MCP Server")
@@ -69,5 +76,40 @@ def get_traces(service: str, last_n_minutes: int):
         return None
 
 
+def create_starlette_app(mcp_server: Server, *, debug: bool = False) -> Starlette:
+    """Create a Starlette application that can server the provied mcp server with SSE."""
+    sse = SseServerTransport("/messages/")
+
+    async def handle_sse(request: Request) -> None:
+        async with sse.connect_sse(
+            request.scope,
+            request.receive,
+            request._send,  # noqa: SLF001
+        ) as (read_stream, write_stream):
+            await mcp_server.run(
+                read_stream,
+                write_stream,
+                mcp_server.create_initialization_options(),
+            )
+
+    return Starlette(
+        debug=debug,
+        routes=[
+            Route("/sse", endpoint=handle_sse),
+            Mount("/messages/", app=sse.handle_post_message),
+        ],
+    )
+
+
 if __name__ == "__main__":
-    mcp.run()
+    mcp_server = mcp._mcp_server  # noqa: WPS437
+
+    parser = argparse.ArgumentParser(description="Run MCP SSE-based server")
+    parser.add_argument("--host", default="127.0.0.1", help="Host to bind to")
+    parser.add_argument("--port", type=int, default=9953, help="Port to listen on")
+    args = parser.parse_args()
+
+    # Bind SSE request handling to MCP server
+    starlette_app = create_starlette_app(mcp_server, debug=True)
+
+    uvicorn.run(starlette_app, host=args.host, port=args.port)
