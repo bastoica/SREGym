3b82085089044e1996faab2287800ccddc7cdc37
Jackson Clark <jclark58@illinois.edu>
2025-06-27 12:40:39 +0200

    resolve issue from merge conflict

--- srearena/conductor/problems/registry.py
+++ srearena/conductor/problems/registry.py
@@ -6,10 +6,11 @@ from srearena.conductor.problems.auth_miss_mongodb import MongoDBAuthMissing
 from srearena.conductor.problems.cart_service_failure import CartServiceFailure
 from srearena.conductor.problems.configmap_drift import ConfigMapDrift
 from srearena.conductor.problems.container_kill import ChaosMeshContainerKill
+from srearena.conductor.problems.env_variable_leak import EnvVariableLeak
 from srearena.conductor.problems.image_slow_load import ImageSlowLoad
 from srearena.conductor.problems.kafka_queue_problems import KafkaQueueProblems
-from srearena.conductor.problems.liveness_probe_too_aggressive import LivenessProbeTooAggressive
 from srearena.conductor.problems.liveness_probe_misconfiguration import LivenessProbeMisconfiguration
+from srearena.conductor.problems.liveness_probe_too_aggressive import LivenessProbeTooAggressive
 from srearena.conductor.problems.loadgenerator_flood_homepage import LoadGeneratorFloodHomepage
 from srearena.conductor.problems.misconfig_app import MisconfigAppHotelRes
 from srearena.conductor.problems.missing_service import MissingService
@@ -29,7 +30,6 @@ from srearena.conductor.problems.scale_pod import ScalePodSocialNet
 from srearena.conductor.problems.service_dns_resolution_failure import ServiceDNSResolutionFailure
 from srearena.conductor.problems.sidecar_port_conflict import SidecarPortConflict
 from srearena.conductor.problems.stale_coredns_config import StaleCoreDNSConfig
-from srearena.conductor.problems.env_variable_leak import EnvVariableLeak
 from srearena.conductor.problems.storage_user_unregistered import MongoDBUserUnregistered
 from srearena.conductor.problems.target_port import K8STargetPortMisconfig
 from srearena.conductor.problems.wrong_bin_usage import WrongBinUsage
@@ -115,10 +115,10 @@ class ProblemRegistry:
                 app_name="hotel_reservation", faulty_service="frontend"
             ),
             "env_variable_leak_social_network": lambda: EnvVariableLeak(
-                app_name="social_network" , faulty_service="media-mongodb"
+                app_name="social_network", faulty_service="media-mongodb"
             ),
             "env_variable_leak_hotel_reservation": lambda: EnvVariableLeak(
-                app_name="hotel_reservation" , faulty_service="mongodb-geo"
+                app_name="hotel_reservation", faulty_service="mongodb-geo"
             ),
             "configmap_drift_hotel_reservation": lambda: ConfigMapDrift(faulty_service="geo"),
             "readiness_probe_misconfiguration_astronomy_shop": lambda: ReadinessProbeMisconfiguration(
