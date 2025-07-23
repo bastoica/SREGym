fbe6f6aabb39635dc775282792b10a558629ef21
Jackson Clark <jclark58@illinois.edu>
2025-07-23 16:36:00 -0500

    Update to use just mitigation oracle

--- srearena/conductor/problems/pod_anti_affinity_deadlock.py
+++ srearena/conductor/problems/pod_anti_affinity_deadlock.py
@@ -2,10 +2,8 @@
 
 import time
 
-from srearena.conductor.oracles.compound import CompoundedOracle
 from srearena.conductor.oracles.localization import LocalizationOracle
 from srearena.conductor.oracles.mitigation import MitigationOracle
-from srearena.conductor.oracles.workload import WorkloadOracle
 from srearena.conductor.problems.base import Problem
 from srearena.generators.fault.inject_virtual import VirtualizationFaultInjector
 from srearena.service.apps.socialnet import SocialNetwork
@@ -20,33 +18,29 @@ class PodAntiAffinityDeadlock(Problem):
         self.namespace = self.app.namespace
         self.faulty_service = faulty_service
         super().__init__(app=self.app, namespace=self.app.namespace)
-        
+
         # === Attach evaluation oracles ===
         self.localization_oracle = LocalizationOracle(problem=self, expected=[self.faulty_service])
 
         # Create workload for evaluation
         self.app.create_workload()
-        self.mitigation_oracle = CompoundedOracle(
-            self,
-            MitigationOracle(problem=self),
-            WorkloadOracle(problem=self, wrk_manager=self.app.wrk),
-        )
+        self.mitigation_oracle = MitigationOracle(problem=self)
 
     @mark_fault_injected
     def inject_fault(self):
         print("== Fault Injection ==")
         print("Creating Pod Anti-Affinity Deadlock...")
         print("Setting requiredDuringScheduling anti-affinity that excludes all nodes")
-        
+
         injector = VirtualizationFaultInjector(namespace=self.namespace)
         injector._inject(
             fault_type="pod_anti_affinity_deadlock",
             microservices=[self.faulty_service],
         )
-        
+
         # Wait for the deadlock to manifest
         time.sleep(30)
-        
+
         print(f"Expected effect: Pods should be in Pending state with:")
         print(f"  '0/X nodes are available: X node(s) didn't match pod anti-affinity rules'")
         print(f"Service: {self.faulty_service} | Namespace: {self.namespace}\n")
@@ -56,11 +50,11 @@ class PodAntiAffinityDeadlock(Problem):
         print("== Fault Recovery ==")
         print("Removing pod anti-affinity deadlock...")
         print("Changing requiredDuring to preferredDuring or removing anti-affinity rules")
-        
+
         injector = VirtualizationFaultInjector(namespace=self.namespace)
         injector._recover(
             fault_type="pod_anti_affinity_deadlock",
             microservices=[self.faulty_service],
         )
-        
+
         print(f"Service: {self.faulty_service} | Namespace: {self.namespace}\n")
