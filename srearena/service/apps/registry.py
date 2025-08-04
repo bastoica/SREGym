e0a1a4f3c2fa85b404947def61cc1aa7e662b063
Jackson Clark <jclark58@illinois.edu>
2025-08-04 01:04:41 -0400

    Add back astronomy shop

--- srearena/service/apps/registry.py
+++ srearena/service/apps/registry.py
@@ -1,18 +1,19 @@
 import json
 
+from srearena.paths import *
 from srearena.service.apps.astronomy_shop import AstronomyShop
 from srearena.service.apps.flight_ticket import FlightTicket
 from srearena.service.apps.hotel_reservation import HotelReservation
 from srearena.service.apps.social_network import SocialNetwork
+from srearena.service.helm import Helm
+
 # from srearena.service.apps.train_ticket import TrainTicket
 
-from srearena.paths import *
-from srearena.service.helm import Helm
 
 class AppRegistry:
     def __init__(self):
         self.APP_REGISTRY = {
-            # "OpenTelemetry Demo Astronomy Shop": AstronomyShop,
+            "Astronomy Shop": AstronomyShop,
             "Flight Ticket": FlightTicket,
             "Hotel Reservation": HotelReservation,
             "Social Network": SocialNetwork,
@@ -20,7 +21,7 @@ class AppRegistry:
         }
 
         self.APP_PATH = {
-            # "OpenTelemetry Demo Astronomy Shop": ASTRONOMY_SHOP_METADATA,
+            "Astronomy Shop": ASTRONOMY_SHOP_METADATA,
             "Flight Ticket": FLIGHT_TICKET_METADATA,
             "Hotel Reservation": HOTEL_RES_METADATA,
             "Social Network": SOCIAL_NETWORK_METADATA,
@@ -32,23 +33,19 @@ class AppRegistry:
             raise ValueError(f"App name {app_name} not found in registry.")
 
         return self.APP_REGISTRY.get(app_name)()
-    
+
     def get_app_names(self):
         return list(self.APP_REGISTRY.keys())
-    
+
     def get_app_config_file(self, app_name: str):
         if app_name not in self.APP_PATH:
             raise ValueError(f"App name {app_name} not found in registry.")
-        
+
         return self.APP_PATH.get(app_name)
-    
+
     def get_app_metadata(self, app_name: str):
         config_file = self.get_app_config_file(app_name)
         with open(config_file, "r") as file:
             metadata = json.load(file)
 
         return metadata
-
-        
-
-
